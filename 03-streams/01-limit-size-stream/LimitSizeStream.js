c2feeda06220c6a0e38ea7147334bc5a1f237730
effervescinn <alinachervonec.98@gmail.com>
2023-02-18 23:03:30 +0300

    03-01 task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,22 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
+    this.dataLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.dataLength += chunk.length;
 
+    if (this.dataLength > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
